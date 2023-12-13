001553a4838db33fd51454fc20c152d76876d512
Paul Mc Auley <paul@roscarraig.com>
2023-12-13 23:38:27 +0000

    Day 14 prelim

---
+++ day14.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import util
+import sys
+
+
+def main():
+    part1 = 0
+    part2 = 0
+
+    for line in util.lines_from_file(sys.argv[1]):
+        pass
+    print(f"Part 1: {part1}")
+    print(f"Part 2: {part2}")
+
+
+if __name__ == '__main__':
+    main()
